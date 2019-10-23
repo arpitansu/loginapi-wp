80ffc07bf8d349e52021c1fc815679bd9d4e0c41
Arpit Pandey <arpit.pandey05@gmail.com>
2019-10-24 00:42:13 +0530

    Update sms.ts

--- src/Utils/sms.ts
+++ src/Utils/sms.ts
@@ -1,6 +1,6 @@
 
-const accountSid = 'AC9793e1395fc410f66946e27d9e442a3a'; // please do not misuse it,  it's just for interview
-const authToken = '1fa38de1a247bc48a00207098107e19b'; // please do not misuse it, it's just for interview
+const accountSid = 'AC9793e1395fc410f66946e27d9e442a3a'; 
+const authToken = '1fa38de1a247bc48a00207098107e19b';
 const client = require('twilio')(accountSid, authToken);
 
 export default class Sms{
